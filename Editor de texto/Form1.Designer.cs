8ef47d030f1a160b76ac83a53a6cec4daacc8b45
54u7 <99465123+54u7@users.noreply.github.com>
2022-07-06 23:05:35 -0400

    Add files via upload

---
+++ Editor de texto/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Editor_de_texto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.openBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CajaTxt = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1470, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newBtn,
+            this.openBtn,
+            this.toolStripSeparator2,
+            this.saveBtn,
+            this.toolStripSeparator3,
+            this.exitBtn});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.fileToolStripMenuItem.Text = "Archivo";
+            // 
+            // newBtn
+            // 
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(180, 22);
+            this.newBtn.Text = "Nuevo";
+            this.newBtn.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openBtn
+            // 
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(180, 22);
+            this.openBtn.Text = "Abrir";
+            this.openBtn.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(180, 22);
+            this.saveBtn.Text = "Guardar";
+            this.saveBtn.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(180, 22);
+            this.exitBtn.Text = "Salir";
+            this.exitBtn.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 645);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // CajaTxt
+            // 
+            this.CajaTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CajaTxt.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CajaTxt.Location = new System.Drawing.Point(0, 24);
+            this.CajaTxt.Name = "CajaTxt";
+            this.CajaTxt.Size = new System.Drawing.Size(1470, 653);
+            this.CajaTxt.TabIndex = 4;
+            this.CajaTxt.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1470, 677);
+            this.Controls.Add(this.CajaTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newBtn;
+        private System.Windows.Forms.ToolStripMenuItem openBtn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem saveBtn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem exitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox CajaTxt;
+    }
+}
+
---
+++ Editor de texto/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Editor_de_texto
+{
+    public partial class Form1 : Form
+    {
+        string archivo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Filter = "Texto|*.txt";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                archivo = open.FileName;
+
+                using (StreamReader sr = new StreamReader(archivo))
+                {
+                    CajaTxt.Text = sr.ReadToEnd();
+                }
+                Form1.ActiveForm.Text = archivo + " | NOTE";
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+
+            save.Filter = "Texto|*.txt";
+
+
+            if (archivo != null) 
+            {
+                using (StreamWriter sw = new StreamWriter(archivo))
+                {
+                    sw.Write(CajaTxt.Text);
+                }
+            }
+            else
+            {
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    archivo = save.FileName;
+                    using (StreamWriter sw = new StreamWriter(save.FileName))
+                    {
+                        sw.Write(CajaTxt.Text);
+                    }
+                }
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CajaTxt.Clear();
+            archivo = null;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
